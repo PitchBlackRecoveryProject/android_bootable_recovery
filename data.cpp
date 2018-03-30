41bc9b036fce491d4d4e246e46bb77ef29d72e44
Manjot Sidhu <manjot.gni@gmail.com>
2018-03-30 19:48:31 +0530

    disable twrp app prompts

--- data.cpp
+++ data.cpp
@@ -890,8 +890,8 @@ void DataManager::SetDefaultValues()
 	mConst.SetValue("tw_oem_build", "1");
 #else
 	mConst.SetValue("tw_oem_build", "0");
-	mPersist.SetValue("tw_app_prompt", "1");
-	mPersist.SetValue("tw_app_install_system", "1");
+	mPersist.SetValue("tw_app_prompt", "0");
+	mPersist.SetValue("tw_app_install_system", "0");
 	mData.SetValue("tw_app_install_status", "0"); // 0 = no status, 1 = not installed, 2 = already installed
 #endif
 
