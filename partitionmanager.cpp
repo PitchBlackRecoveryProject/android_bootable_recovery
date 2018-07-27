3f7d0bc2115f8358e67abec9e605b1c9ad8dde60
Captain Throwback <captainthrowback@gmail.com>
2018-07-27 10:05:24 -0400

    twrp: use ANDROID_ROOT environment variable
    instead of hard-coded /system path
    
    I updated most of the references I found,
    but there might be more
    
    For devices that have to mount system at /system_root,
    this allows system to be bind mounted to /system
    and detected properly by TWRP
    
    Change-Id: I9f142fd8cec392f5b88e95476258dab9c21a9aac

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -977,6 +977,7 @@ TWPartitionManager::Run_Backup (bool adbbackup)
 {
   PartitionSettings part_settings;
   int partition_count = 0, disable_free_space_check = 0, skip_digest = 0;
+  int gui_adb_backup;
   string Backup_Name, Backup_List, backup_path;
   unsigned long long total_bytes = 0, free_space = 0;
   TWPartition *storage = NULL;
@@ -1446,7 +1447,7 @@ TWPartitionManager::Run_Restore (const string & Restore_Name)
     }
   TWFunc::GUI_Operation_Text (TW_UPDATE_SYSTEM_DETAILS_TEXT,
 			      gui_parse_text ("{@updating_system_details}"));
-  UnMount_By_Path ("/system", false);
+  UnMount_By_Path (Get_Android_Root_Path(), false);
   Update_System_Details ();
   UnMount_Main_Partitions ();
   time (&rStop);
@@ -2580,7 +2581,7 @@ TWPartitionManager::UnMount_Main_Partitions (void)
 
   TWPartition *Boot_Partition = Find_Partition_By_Path ("/boot");
 
-  UnMount_By_Path ("/system", true);
+  UnMount_By_Path (Get_Android_Root_Path(), true);
   if (!datamedia)
     UnMount_By_Path ("/data", true);
 
@@ -3797,6 +3798,13 @@ TWPartitionManager::Get_Active_Slot_Display ()
   return Active_Slot_Display;
 }
 
+string TWPartitionManager::Get_Android_Root_Path() {
+	std::string Android_Root = getenv("ANDROID_ROOT");
+	if (Android_Root == "")
+		Android_Root = "/system";
+	return Android_Root;
+}
+
 void
 TWPartitionManager::Remove_Uevent_Devices (const string & Mount_Point)
 {
@@ -4539,7 +4547,7 @@ TWPartitionManager::Run_OTA_Survival_Restore (const string & Restore_Name)
 	  end_pos = Restore_List.find (";", start_pos);
 	}
     }
-  UnMount_By_Path ("/system", false);
+  UnMount_By_Path (Get_Android_Root_Path(), false);
   Update_System_Details_OTA_Survival ();
   UnMount_Main_Partitions ();
   time (&rStop);
