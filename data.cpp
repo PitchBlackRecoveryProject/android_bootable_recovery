639c24c7871373e54d174a092d04311e8b34cd83
bigbiff bigbiff <bigbiff@teamw.in>
2018-09-19 19:13:19 -0400

    Fix sha2 generation persistence.
    Change-Id: I8342ee36f996a0305632bdb6eb8b20ae4e93873b

--- data.cpp
+++ data.cpp
@@ -805,10 +805,10 @@ void DataManager::SetDefaultValues()
 	mPersist.SetValue("tw_military_time", "0");
 
 #ifdef TW_INCLUDE_CRYPTO
-	mConst.SetValue(TW_USE_SHA2, "1");
-	mConst.SetValue(TW_NO_SHA2, "0");
+	mPersist.SetValue(TW_USE_SHA2, "1");
+	mPersist.SetValue(TW_NO_SHA2, "0");
 #else
-	mConst.SetValue(TW_NO_SHA2, "1");
+	mPersist.SetValue(TW_NO_SHA2, "1");
 #endif
         mPersist.SetValue(TRB_EN, "0");
         mPersist.SetValue(STD, "0");
