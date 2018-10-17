f5a28af24f4f2f6e9ab94276302e83139543a911
Ethan Yonker <dees_troy@teamw.in>
2018-10-17 08:39:28 -0500

    WIP: Update FDE decrypt to pie from CAF
    cryptfs.cpp based on CAF tag LA.UM.7.3.r1-05900-sdm845.0
    Used CAF because AOSP no longer contains code for qcom's hardware
    crypto.
    
    Change-Id: I921cbe9bed70989f91449e23b5ac3ec1037b7b97

--- partition.cpp
+++ partition.cpp
@@ -47,6 +47,9 @@
 #include "set_metadata.h"
 #include "gui/gui.hpp"
 #include "adbbu/libtwadbbu.hpp"
+#ifdef TW_INCLUDE_CRYPTO
+	#include "crypto/fde/cryptfs.h"
+#endif
 extern "C" {
 	#include "mtdutils/mtdutils.h"
 	#include "mtdutils/mounts.h"
@@ -56,7 +59,7 @@ extern "C" {
 #endif
 
 #ifdef TW_INCLUDE_CRYPTO
-	#include "crypto/lollipop/cryptfs.h"
+	//#include "crypto/lollipop/cryptfs.h"
 	#include "gpt/gpt.h"
 	#ifdef TW_INCLUDE_FBE
 		#include "crypto/ext4crypt/Decrypt.h"
