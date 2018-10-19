2da20730d3fd39e00dd2f4ed4182b08993e6caef
Ethan Yonker <dees_troy@teamw.in>
2018-10-19 08:06:03 -0500

    Revert "minuitwrp: Fix detection of libdrm on Pie"
    This reverts commit 8b7a5672bcc1a70cf3d3f78b0f3f1c2f1a87f4d4.

--- minuitwrp/Android.mk
+++ minuitwrp/Android.mk
@@ -51,7 +51,7 @@ ifeq ($(TW_NEW_ION_HEAP), true)
   LOCAL_CFLAGS += -DNEW_ION_HEAP
 endif
 
-ifneq ($(wildcard external/libdrm/Android.*),)
+ifneq ($(wildcard external/libdrm/Android.mk),)
   LOCAL_CFLAGS += -DHAS_DRM
   LOCAL_SRC_FILES += graphics_drm.cpp
   ifneq ($(wildcard external/libdrm/Android.common.mk),)
