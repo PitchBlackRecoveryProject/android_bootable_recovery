d4c7f317b414f59f349d201eb1b2b2defb3e614e
Manjot Sidhu <manjot.techie@gmail.com>
2019-11-04 23:26:59 +0530

    Fix typo * Thanks @dhohasaizo for this

--- twinstall.cpp
+++ twinstall.cpp
@@ -629,7 +629,7 @@ int TWinstall_zip(const char* path, int* wipe_cache) {
 		LOGINFO("Update binary zip\n");
 		// Additionally verify the compatibility of the package.
 		if (!verify_package_compatibility(&Zip) && DataManager::GetIntValue(PB_TREBLE_COMP) == 0) {
-			gui_err("zip_compatible_err=Zip Treble compatibility error!");
+			gui_err("zip_err_compatibility=Zip Treble compatibility error!");
 			Zip.Close();
 #ifdef USE_MINZIP
 			sysReleaseMap(&map);
