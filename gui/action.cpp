6331f3696cecb34ca1373219b9474de36e3296bc
Mohd Faraz <androiabledroid@gmail.com>
2020-01-12 00:51:59 +0530

    PBRP: gui: Fix up codename
    Change-Id: Ibd01199ca087aee1fcd684834b6034756833950c
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- gui/action.cpp
+++ gui/action.cpp
@@ -2457,78 +2457,74 @@ int GUIAction::change_codename(std::string arg __unused)
 {
 	int op_status = 1;
 	operation_start("Codename Changing");
-	if (!simulate)
+	string codename, new_codename, part_name, null;
+	int values = 1;
+	DataManager::GetValue(PB_PROP_VALUE, codename);
+	DataManager::SetProgress(0);
+	DataManager::GetValue("pb_codename_change", new_codename);
+	//Removal of Newlines from values
+	codename.erase(std::remove(codename.begin(), codename.end(), '\n'), codename.end());
+	new_codename.erase(std::remove(new_codename.begin(), new_codename.end(), '\n'), new_codename.end());
+	if (!DataManager::GetIntValue("tw_has_boot_slots"))
 	{
-		string codename, new_codename, part_name, null;
-		int values = 1;
-		DataManager::GetValue(PB_PROP_VALUE, codename);
-		DataManager::SetProgress(0);
-		DataManager::GetValue("pb_codename_change", new_codename);
-		//Removal of Newlines from values
-		codename.erase(std::remove(codename.begin(), codename.end(), '\n'), codename.end());
-		new_codename.erase(std::remove(new_codename.begin(), new_codename.end(), '\n'), new_codename.end());
-		if (!DataManager::GetIntValue("tw_has_boot_slots"))
+		part_name = "/recovery";
+		values = 1;
+	}
+	else {
+		part_name = "/boot";
+		values = 2;
+	}
+	DataManager::SetProgress(.10);
+	for(int i=1; i<=values; i++)
+	{
+		if (values == 2)
 		{
-			part_name = "/recovery";
-			values = 1;
-		}
-		else {
-			part_name = "/boot";
-			values = 2;
-		}
-		DataManager::SetProgress(.10);
-		for(int i=1; i<=values; i++)
-		{
-			if (values == 2)
-			{
-				string Current_Slot = PartitionManager.Get_Active_Slot_Display();
-				if (Current_Slot == "A")
-					PartitionManager.Set_Active_Slot("B");
-				else
-					PartitionManager.Set_Active_Slot("A");
-				if (i==1)
-				DataManager::SetProgress(.15);
-				else
-				DataManager::SetProgress(.50);
-			}
+			string Current_Slot = PartitionManager.Get_Active_Slot_Display();
+			if (Current_Slot == "A")
+				PartitionManager.Set_Active_Slot("B");
 			else
-				DataManager::SetProgress(.50);
-			TWFunc::Unpack_Image(part_name);
-			string name, split_img = "/tmp/pb/ramdisk";
-			if (TWFunc::Path_Exists(split_img))
+				PartitionManager.Set_Active_Slot("A");
+			if (i==1)
+			DataManager::SetProgress(.15);
+			else
+			DataManager::SetProgress(.50);
+		}
+		else
+			DataManager::SetProgress(.50);
+		TWFunc::Unpack_Image(part_name);
+		string name, split_img = "/tmp/pb/ramdisk";
+		if (TWFunc::Path_Exists(split_img))
+		{
+			DIR* dir;
+			struct dirent* der;
+			dir = opendir(split_img.c_str());
+			while((der = readdir(dir)) != NULL)
 			{
-				DIR* dir;
-				struct dirent* der;
-				dir = opendir(split_img.c_str());
-				while((der = readdir(dir)) != NULL)
+				name = der->d_name;
+				if (name == "default.prop")
 				{
-					name = der->d_name;
-					if (name == "default.prop")
-					{
-						string command = "sed -i \'s|ro.product.device="+codename+"|ro.product.device="+new_codename+"|g\' "+ split_img + "/" + name;
-						TWFunc::Exec_Cmd(command, null);
-					}
+					string command = "sed -i \'s|ro.product.device="+codename+"|ro.product.device="+new_codename+"|g\' "+ split_img + "/" + name;
+					TWFunc::Exec_Cmd(command, null);
 				}
-				closedir(dir);
 			}
-			else
-			{
-				LOGINFO("PBRP: Ramdisk Doesnt Exists \n");
-				goto exit;
-			}
-			TWFunc::Repack_Image(part_name);
-			if (values == 1)
-				DataManager::SetProgress(1);
-			else {
-				if (i==1 && values == 2)
-					DataManager::SetProgress(.25);
-				else
-					DataManager::SetProgress(1);
-			}
-
+			closedir(dir);
 		}
-	} else
-		simulate_progress_bar();
+		else
+		{
+			LOGINFO("PBRP: Ramdisk Doesnt Exists \n");
+			goto exit;
+		}
+		TWFunc::Repack_Image(part_name);
+		if (values == 1)
+			DataManager::SetProgress(1);
+		else {
+			if (i==1 && values == 2)
+				DataManager::SetProgress(.25);
+			else
+				DataManager::SetProgress(1);
+		}
+
+	}
 	op_status = 0;
 exit:
 	operation_end(op_status);
@@ -2537,13 +2533,9 @@ exit:
 
 int GUIAction::getprop(std::string arg)
 {
-	if (!simulate) {
-		string value;
-		TWFunc::Exec_Cmd("getprop " + arg, value);
-		DataManager::SetValue(PB_PROP_VALUE, value);
-	}
-	else
-		simulate_progress_bar();
+	string value;
+	TWFunc::Exec_Cmd("getprop " + arg, value);
+	DataManager::SetValue(PB_PROP_VALUE, value);
 	return 0;
 }
 
