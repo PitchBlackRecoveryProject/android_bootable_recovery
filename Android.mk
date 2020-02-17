e8771ad7cf5266aec008c58251a7d3250339cbb1
Mohd Faraz <androiabledroid@gmail.com>
2020-02-17 13:37:51 +0530

    Revert "PBRP: Bring back some lefts props"
    This reverts commit f92f8fc47f4346304d4e56f58e9a8cae50d8ca08.
    
     * Now this is controlled by vendor
    
    Change-Id: I3376907db067257304e53a83b2ae326af8459895
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- Android.mk
+++ Android.mk
@@ -456,8 +456,7 @@ TWRP_REQUIRED_MODULES += \
     simg2img_twrp \
     libbootloader_message_twrp \
     init.recovery.hlthchrg.rc \
-    init.recovery.service.rc \
-    startup.sh
+    init.recovery.service.rc
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26; echo $$?),0)
 TWRP_REQUIRED_MODULES += \
     init.recovery.ldconfig.rc
