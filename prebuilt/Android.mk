1252842f16ebf2a584f74fd9fd30a099bb4ec7b1
Mohd Faraz <androiabledroid@gmail.com>
2020-02-17 13:37:51 +0530

    Revert "PBRP: Bring back some lefts props"
    This reverts commit f92f8fc47f4346304d4e56f58e9a8cae50d8ca08.
    
     * Now this is controlled by vendor
    
    Change-Id: I3376907db067257304e53a83b2ae326af8459895
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -682,14 +682,6 @@ else
 endif
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := startup.sh
-LOCAL_MODULE_TAGS := eng
-LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
-LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
 ifeq ($(TW_INCLUDE_CRYPTO), true)
     ifneq ($(TW_CRYPTO_USE_SYSTEM_VOLD),)
         ifneq ($(shell test $(PLATFORM_SDK_VERSION) -ge 28; echo $$?),0)
