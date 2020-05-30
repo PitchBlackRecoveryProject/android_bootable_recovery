23f15a606ec94fbe4e3fc9696ad16fad73d79934
Mohd Faraz <mohd.faraz.abc@gmail.com>
2020-05-30 21:06:32 +0530

    PBRP: Enable qti input haptics vibrators support
     * these type of vibrators are present in newer devices based on board sm8150 & above. Like Oneplus 7 series, Redmi K20 Pro, etc.
    
    Change-Id: I0fe6612def149e70808ca41829b6f7ba0b23cd62
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- minuitwrp/events.cpp
+++ minuitwrp/events.cpp
@@ -28,6 +28,10 @@
 #include <string.h>
 #include <fstream>
 
+#ifdef USE_QTI_HAPTICS
+#include <android/hardware/vibrator/1.2/IVibrator.h>
+#endif
+
 #include "../common.h"
 
 #include "minui.h"
@@ -130,12 +134,18 @@ int vibrate(int timeout_ms)
     char tout[6];
     sprintf(tout, "%i", timeout_ms);
 
+#ifndef USE_QTI_HAPTICS
     if (std::ifstream(LEDS_HAPTICS_ACTIVATE_FILE).good()) {
         write_to_file(LEDS_HAPTICS_DURATION_FILE, std::to_string(timeout_ms));
         write_to_file(LEDS_HAPTICS_ACTIVATE_FILE, "1");
     } else
         write_to_file(VIBRATOR_TIMEOUT_FILE, std::to_string(timeout_ms));
-
+#else
+    android::sp<android::hardware::vibrator::V1_2::IVibrator> vib = android::hardware::vibrator::V1_2::IVibrator::getService();
+    if (vib != nullptr) {
+        vib->on((uint32_t)timeout_ms);
+    }
+#endif
     return 0;
 }
 #endif
