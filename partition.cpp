81181ed2e7e675558d8bddc75151e254702fb0b1
rezaadi0105 <rezaadipangestu5@gmail.com>
2020-08-23 15:02:19 +0700

    partititon: Exclude more dir for backup
    fixed error 255 when backup
    
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>
    Change-Id: Ica408fdc21d19194ed75c3f31d7ebc9364fee61f
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- partition.cpp
+++ partition.cpp
@@ -1213,6 +1213,14 @@ void TWPartition::Setup_Data_Media() {
 		backup_exclusions.add_absolute_dir("/data/data/com.google.android.music/files");
 		backup_exclusions.add_absolute_dir("/data/per_boot"); // DJ9,14Jan2020 - exclude this dir to prevent "error 255" on AOSP ROMs that create and lock it
 		backup_exclusions.add_absolute_dir("/data/vendor/dumpsys");
+		backup_exclusions.add_absolute_dir("/data/vendor_ce/10");
+		backup_exclusions.add_absolute_dir("/data/vendor_ce/999");
+		backup_exclusions.add_absolute_dir("/data/system_ce/10");
+		backup_exclusions.add_absolute_dir("/data/system_ce/999");
+		backup_exclusions.add_absolute_dir("/data/misc_ce/10");
+		backup_exclusions.add_absolute_dir("/data/misc_ce/999");
+		backup_exclusions.add_absolute_dir("/data/user/10");
+		backup_exclusions.add_absolute_dir("/data/user/999");
 		backup_exclusions.add_absolute_dir("/data/cache");
 		wipe_exclusions.add_absolute_dir(Mount_Point + "/misc/vold"); // adopted storage keys
 		ExcludeAll(Mount_Point + "/system/storage.xml");
