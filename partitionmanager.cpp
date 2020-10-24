014fefdfae5cca0009c6bec6f4f6906b81673563
Captain Throwback <captainthrowback@hotmail.com>
2020-10-24 17:17:09 -0400

    fde: set ro.crypto.type to block
    Since we set ro.crypto.type to "file" for FBE, we should also set
    this prop for FDE.
    
    Change-Id: Id3a581da5d4b58c41c2c361593ad7266898a6f54
    Signed-off-by: AndroiableDroid <androiabledroid@gmail.com>

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -2570,7 +2570,8 @@ int TWPartitionManager::Decrypt_Device(string Password, int user_id) {
 	if (strcmp (crypto_state, "error") == 0)
 	{
 		property_set ("ro.crypto.state", "encrypted");
-// Sleep for a bit so that services can start if needed
+		property_set("ro.crypto.type", "block");
+		// Sleep for a bit so that services can start if needed
 		sleep (1);
 	}
 
