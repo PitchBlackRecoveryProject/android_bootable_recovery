95b4cf476666d8da4b862a3e70db7b3f6e93f8cb
Captain Throwback <captainthrowback@hotmail.com>
2020-10-29 12:43:17 -0400

    fde: move crypto state & type setting to function
    In order to make sure the crypto state and type is set
    properly if the device is encrypted, move this check to
    a function and call it in cases where the default password
    is being used and in cases where it isn't. This will ensure that
    an unencrypted device isn't set as encrypted by mistake.
    
    Change-Id: I736f9cbee0e95d7390a09c8235b9914feddaceaf

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -143,6 +143,14 @@ TWPartitionManager::TWPartitionManager(void) {
 #endif
 }
 
+int TWPartitionManager::Set_FDE_Encrypt_Status(void) {
+	property_set("ro.crypto.state", "encrypted");
+	property_set("ro.crypto.type", "block");
+	// Sleep for a bit so that services can start if needed
+	sleep(1);
+	return 0;
+}
+
 int
 TWPartitionManager::Process_Fstab (string Fstab_Filename, bool Display_Error, bool Sar_Detect)
 {
@@ -510,6 +518,7 @@ void TWPartitionManager::Decrypt_Data() {
 		}
 		else
 		{
+			Set_FDE_Encrypt_Status();
 			int password_type = cryptfs_get_password_type ();
 			if (password_type == CRYPT_TYPE_DEFAULT)
 			{
@@ -2566,13 +2575,9 @@ int TWPartitionManager::Decrypt_Device(string Password, int user_id) {
 	}
 	property_set ("twrp.mount_to_decrypt", "1");
 
-	property_get ("ro.crypto.state", crypto_state, "error");
-	if (strcmp (crypto_state, "error") == 0)
-	{
-		property_set ("ro.crypto.state", "encrypted");
-		property_set("ro.crypto.type", "block");
-		// Sleep for a bit so that services can start if needed
-		sleep (1);
+	property_get("ro.crypto.state", crypto_state, "error");
+	if (strcmp(crypto_state, "error") == 0) {
+		Set_FDE_Encrypt_Status();
 	}
 
 	if (DataManager::GetIntValue (TW_IS_FBE))
--- partitions.hpp
+++ partitions.hpp
@@ -402,6 +402,7 @@ public:
 	bool Recreate_Logs_Dir();                                                 // Recreate TWRP_AB_LOGS_DIR after wipe
 	void Change_System_Root(bool root);
 	std::vector<users_struct>* Get_Users_List();                              // Returns pointer to list of users
+	int Set_FDE_Encrypt_Status();                                             // Sets encryption state for FDE devices (ro.crypto.state and ro.crypto.type)
 
 private:
 	void Setup_Settings_Storage_Partition(TWPartition* Part);                 // Sets up settings storage
