ff546a3293bbc1f33b7de91d6fb47f006c31ca5e
Manjot Sidhu <manjot.techie@gmail.com>
2021-05-11 05:13:19 +0000

    PBRP: Backup: Missing else conditions

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -2176,27 +2176,31 @@ TWPartitionManager::Wipe_Media_From_Data (void)
 			{
 				LOGINFO ("Unable to open '%s'\n", dest_t.c_str ());
 			}
-			while (pent == readdir (pdir))
-			{
-				o_file = src_t + "/" + pent->d_name + "";
-				c_file = dest_t + "/" + pent->d_name + "";
-				TWFunc::copy_file (c_file, o_file, 0777);
-			}
-			closedir (pdir);
+			else {
+				while (pent == readdir (pdir))
+				{
+					o_file = src_t + "/" + pent->d_name + "";
+					c_file = dest_t + "/" + pent->d_name + "";
+					TWFunc::copy_file (c_file, o_file, 0777);
+				}
+				closedir (pdir);
 
-			pdir = opendir (dst_ar_m.c_str ());
-			if (pdir == NULL)
-			{
-				LOGINFO ("Unable to open '%s'\n", dst_ar_m.c_str ());
-			}
-			while (pent == readdir (pdir))
-			{
+				pdir = opendir (dst_ar_m.c_str ());
+				if (pdir == NULL)
+				{
+					LOGINFO ("Unable to open '%s'\n", dst_ar_m.c_str ());
+				}
+				else {
+					while (pent == readdir (pdir))
+					{
 
-				o_file = src_ar_m + "/" + pent->d_name + "";
-				c_file = dst_ar_m + "/" + pent->d_name + "";
-				TWFunc::copy_file (c_file, o_file, 0777);
+						o_file = src_ar_m + "/" + pent->d_name + "";
+						c_file = dst_ar_m + "/" + pent->d_name + "";
+						TWFunc::copy_file (c_file, o_file, 0777);
+					}
+					closedir (pdir);
+				}
 			}
-			closedir (pdir);
 		}
 
 		return true;
