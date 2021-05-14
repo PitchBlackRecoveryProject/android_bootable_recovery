413a57eef85d765c00889044b9b06fb35dd240cf
bigbiff <bigbiff@teamw.in>
2021-05-14 19:51:26 -0400

    fastboot: allow device to start required healthd and boot-hal services
    Change-Id: Iac1d20ce26d49e7a99071c968d8c8e654e5ff1c6

--- Android.mk
+++ Android.mk
@@ -188,6 +188,7 @@ ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS),true)
         LOCAL_CFLAGS += -DMORE_LOGICAL='"$(TW_INCLUDE_LOGICAL)"'
     endif
     TWRP_REQUIRED_MODULES += android.hardware.health@2.1-service android.hardware.health@2.1-impl.recovery android.hardware.health@2.1-service.rc android.hardware.health@2.1.xml
+    TWRP_REQUIRED_MODULES += android.hardware.health@2.0-service android.hardware.health@2.0-impl.recovery android.hardware.health@2.0-service.rc
 endif
 
 ifneq ($(BOARD_GOOGLE_DYNAMIC_PARTITIONS_PARTITION_LIST),)
--- etc/Android.mk
+++ etc/Android.mk
@@ -98,6 +98,14 @@ ifeq ($(filter $(AB_OTA_UPDATER) $(PRODUCT_USE_DYNAMIC_PARTITIONS), true), true)
 	LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/vendor/etc/vintf/manifest
 	LOCAL_SRC_FILES := init/$(LOCAL_MODULE)
 	include $(BUILD_PREBUILT)
+
+	include $(CLEAR_VARS)
+	LOCAL_MODULE := android.hardware.health@2.0-service.rc
+	LOCAL_MODULE_TAGS := optional
+	LOCAL_MODULE_CLASS := EXECUTABLES
+	LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/etc/init
+	LOCAL_SRC_FILES := init/$(LOCAL_MODULE)
+	include $(BUILD_PREBUILT)
 endif
 
 ifeq ($(AB_OTA_UPDATER),true)
