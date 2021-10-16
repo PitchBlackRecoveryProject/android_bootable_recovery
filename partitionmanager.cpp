09555f095e94e2dddcc54414461cf6bcc6b5b8e1
Captain Throwback <captainthrowback@hotmail.com>
2021-10-16 16:16:02 -0400

    gui: allow MTP even if media storage isn't present
    Change-Id: I6766f804fa7f116ddc436d566d9647206b0d2e69

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -3440,12 +3440,6 @@ bool
 TWPartitionManager::Enable_MTP (void)
 {
 #ifdef TW_HAS_MTP
-    string mtp_base_path = "/data/media";
-    if (!TWFunc::Path_Exists(mtp_base_path)) {
-		gui_msg(Msg(msg::kWarning, "mtp_base_path_missing=Media storage not found. Unable to enable MTP."));
-		return false;
-	}
-
 	if (mtppid) {
 		gui_err("mtp_already_enabled=MTP already enabled");
 		return true;
