d9470bc2a7415ccf968cdba1e0db3c2563cdd3f9
Yifan Hong <elsk@google.com>
2021-11-30 16:39:00 -0800

    GetBatteryInfo() also reads AIDL health HAL.
    Test: call GetBatteryInfo manually with and without AIDL health HAL
    Bug: 170338625
    Bug: 177269435
    Change-Id: I123739e5bc372d5198fd711f592ceac04d46ab28

--- Android.mk
+++ Android.mk
@@ -106,7 +106,7 @@ else
 endif
 
 LOCAL_STATIC_LIBRARIES += libavb libtwrpinstall libminadbd_services libinit libsnapshot_nobinder update_metadata-protos librecovery_utils libhealthhalutils
-LOCAL_SHARED_LIBRARIES += libfs_mgr libhardware android.hardware.boot@1.0 android.hardware.boot@1.1 android.hardware.boot@1.2 libprotobuf-cpp-lite liblp libutils libhidlbase android.hardware.health@2.0
+LOCAL_SHARED_LIBRARIES += libfs_mgr libhardware android.hardware.boot@1.0 android.hardware.boot@1.1 android.hardware.boot@1.2 libprotobuf-cpp-lite liblp libutils libhidlbase android.hardware.health-V1-ndk
 LOCAL_C_INCLUDES += \
     system/core/fs_mgr/libfs_avb/include/ \
     system/core/fs_mgr/include_fstab/ \
