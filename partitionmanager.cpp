0b4892ca9884aa665d84b802db002b0c043a80ff
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-28 04:07:04 +0800

    partitionmanager: Only create logical partition symlink when it doesn't exist
    Legacy devices might have symlinks to real partitions in this folder.
    
    Change-Id: I4d3360ff1022eabbd4f6ae7688d8bb7306ae9777

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -4077,11 +4077,10 @@ bool TWPartitionManager::Prepare_Super_Volume(TWPartition* twrpPart) {
 	twrpPart->Set_Can_Be_Backed_Up(false);
 	twrpPart->Set_Can_Be_Wiped(false);
 	twrpPart->Update_Size(true);
-	LOGINFO("Symlinking %s => /dev/block/bootdevice/by-name/%s \n", fstabEntry.blk_device.c_str(),
-					bare_partition_name.c_str());
-	symlink(fstabEntry.blk_device.c_str(),
-					("/dev/block/bootdevice/by-name/" + bare_partition_name).c_str());
-
+	if (access(("/dev/block/bootdevice/by-name/" + bare_partition_name).c_str(), F_OK) == -1) {
+		LOGINFO("Symlinking %s => /dev/block/bootdevice/by-name/%s \n", fstabEntry.blk_device.c_str(), bare_partition_name.c_str());
+		symlink(fstabEntry.blk_device.c_str(), ("/dev/block/bootdevice/by-name/" + bare_partition_name).c_str());
+	}
 	return true;
 }
 
