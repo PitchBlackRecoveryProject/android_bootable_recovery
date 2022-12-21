da38df046282d14daffb264abd475e49cd2c2682
AndroiableDroid <AndroiableDroid@users.noreply.github.com>
2022-12-21 05:52:26 +0000

    PBRP: Bump v3.2.0 -> v4.0
    Signed-off-by: SathamHussainM <sathamhussain.m11@gmail.com>

--- variables.h
+++ variables.h
@@ -18,7 +18,7 @@
 #ifndef _VARIABLES_HEADER_
 #define _VARIABLES_HEADER_
 
-#define PB_MAIN_VERSION              "3.2.0"
+#define PB_MAIN_VERSION              "4.0"
 #define PB_BUILD PB_MAIN_VERSION PB_MAIN_BUILD
 #define PB_DEVICE               PB_DEVICE_MODEL
 
