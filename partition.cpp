292c11d9c7282afa1bffcb59e7af91b6fad44f9c
me-cafebabe <me.cafebabe@gmail.com>
2023-07-30 18:25:33 +0800

    partition: Exclude /data/gsi directory from userdata backup
    Change-Id: Iebf60b0f352803cef12267f17332e9f63bb58130
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- partition.cpp
+++ partition.cpp
@@ -1268,6 +1268,7 @@ void TWPartition::Setup_Data_Media() {
 		backup_exclusions.add_absolute_dir("/data/cache");
         backup_exclusions.add_absolute_dir("/data/misc/apexdata/com.android.art"); // exclude this dir to prevent "error 255" on AOSP Android 12
 		backup_exclusions.add_absolute_dir("/data/extm"); //exclude this dir to prevent "error 255" on MIUI
+		backup_exclusions.add_absolute_dir("/data/gsi"); // Contains huge files (DSU System image + Userdata image), and won't work after restoration (requires configuration files in metadata)
 		wipe_exclusions.add_absolute_dir(Mount_Point + "/misc/vold"); // adopted storage keys
 		ExcludeAll(Mount_Point + "/system/storage.xml");
 
