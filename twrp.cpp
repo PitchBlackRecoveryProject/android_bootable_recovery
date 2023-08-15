bd6e47ba9f39e988fbf5324a8751f1d019daec28
micky387 <mickaelsaibi@free.fr>
2023-08-15 15:32:11 +0200

    twrp.cpp: Add braces for fastboot mode
    Without brace, only the line until ";" is guarded
    
    Change-Id: I5483840ddcc2731cffa8552ca69730a1a3a47a69
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- twrp.cpp
+++ twrp.cpp
@@ -422,10 +422,11 @@ int main(int argc, char **argv) {
 	}
 
 #ifdef TW_LOAD_VENDOR_MODULES
-	if (startup.Get_Fastboot_Mode())
-	android::base::SetProperty("ro.boot.fastboot", "1");
+	if (startup.Get_Fastboot_Mode()) {
+		android::base::SetProperty("ro.boot.fastboot", "1");
 		PartitionManager.Prepare_Super_Volume(PartitionManager.Find_Partition_By_Path("/vendor"));
 		PartitionManager.Prepare_Super_Volume(PartitionManager.Find_Partition_By_Path("/vendor_dlkm"));
+	}
 	KernelModuleLoader::Load_Vendor_Modules();
 #endif
 	printf("Starting the UI...\n");
