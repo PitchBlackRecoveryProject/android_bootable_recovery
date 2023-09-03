bd2ae40975381d878594ec4a944703f2bc014490
Mohd Faraz <androiabledroid@gmail.com>
2023-09-04 01:31:25 +0700

    Revert "twrp.cpp: move up gui_init in startup process"
    Reason: Loading prior kernel modules, this is causing issue with split msm_drm modules
    
    This reverts commit 2178ad6686adc1a3cf14dd8f91ed120e8fcd3bb5.

--- twrp.cpp
+++ twrp.cpp
@@ -400,11 +400,6 @@ int main(int argc, char **argv) {
 
 	// Load default values to set DataManager constants and handle ifdefs
 	DataManager::SetDefaultValues();
-
-	// Show PBRP splash here
-	printf("Starting the UI...\n");
-	gui_init();
-
         startupArgs startup;
         startup.parse(&argc, &argv);
 	android::base::SetProperty(TW_FASTBOOT_MODE_PROP, startup.Get_Fastboot_Mode() ? "1" : "0");
@@ -432,6 +427,8 @@ int main(int argc, char **argv) {
 		}
 	}
 #endif
+	printf("Starting the UI...\n");
+	gui_init();
 
 	// Load up all the resources
 	gui_loadResources();
