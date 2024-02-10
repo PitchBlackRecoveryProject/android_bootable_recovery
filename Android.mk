3f9424de255f4880aafd84c29b7007626cc5d33a
Captain Throwback <captainthrowback@hotmail.com>
2024-02-10 12:56:08 -0500

    brightness: default is provided by vendor_pb
    Change-Id: If986b8ece21fdaa4c7f9c7bceaf1d5e2f70c1016
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- Android.mk
+++ Android.mk
@@ -390,12 +390,6 @@ ifneq ($(TW_SECONDARY_BRIGHTNESS_PATH),)
 endif
 ifneq ($(TW_MAX_BRIGHTNESS),)
 	LOCAL_CFLAGS += -DTW_MAX_BRIGHTNESS=$(TW_MAX_BRIGHTNESS)
-else
-    ifneq ($(TW_NO_SCREEN_BLANK),)
-        TW_MAX_BRIGHTNESS ?= 1023
-        LOCAL_CFLAGS += -DTW_MAX_BRIGHTNESS=$(TW_MAX_BRIGHTNESS)
-        $(call pretty-warning,TW_MAX_BRIGHTNESS must be defined if using TW_NO_SCREEN_BLANK. Setting to default value: $(TW_MAX_BRIGHTNESS). If you would like to use a different value then please update your device tree with the correct value from your device.)
-    endif
 endif
 ifneq ($(TW_DEFAULT_BRIGHTNESS),)
 	LOCAL_CFLAGS += -DTW_DEFAULT_BRIGHTNESS=$(TW_DEFAULT_BRIGHTNESS)
