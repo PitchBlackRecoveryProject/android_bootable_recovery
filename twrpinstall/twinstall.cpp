a81356687772f3d3b6c16a6c5772440ef4ddf1e3
sekaiacg <sekaiacg@gmail.com>
2024-02-18 15:09:10 +0700

    twinstall: Show how long the installation took
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- twrpinstall/twinstall.cpp
+++ twrpinstall/twinstall.cpp
@@ -621,5 +621,8 @@ int TWinstall_zip(const char* path, int* wipe_cache, bool check_for_digest) {
 					DataManager::SetValue(PB_LOADED_FINGERPRINT, 0);
 				LOGINFO("Install took %i second(s).\n", total_time);
 	}
+
+	if (ret_val == INSTALL_SUCCESS) gui_msg(Msg(msg::kHighlight, "install_took_seconds_msg=Install took {1} second(s).")(total_time));
+
 	return ret_val;
 }
