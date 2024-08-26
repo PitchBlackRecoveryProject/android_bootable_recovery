89c8f9cef283bd8232fda03aa231cbf3405673de
bigbiff <bigbiff@teamw.in>
2024-08-26 19:14:00 -0400

    twrp sepolicy: move to system/sepolicy
    Change-Id: Ie81631499a6f44ee1539749cf3b5c4132a317f65

--- Android.mk
+++ Android.mk
@@ -16,27 +16,6 @@
 LOCAL_PATH := $(call my-dir)
 commands_TWRP_local_path := $(LOCAL_PATH)
 
-ifneq ($(project-path-for),)
-    ifeq ($(LOCAL_PATH),$(call project-path-for,recovery))
-        PROJECT_PATH_AGREES := true
-        BOARD_SEPOLICY_DIRS += $(call project-path-for,recovery)/sepolicy
-    endif
-else
-    ifeq ($(LOCAL_PATH),bootable/recovery)
-        PROJECT_PATH_AGREES := true
-        BOARD_SEPOLICY_DIRS += bootable/recovery/sepolicy
-    else
-        ifeq ($(LOCAL_PATH),bootable/recovery-twrp)
-            ifeq ($(RECOVERY_VARIANT),twrp)
-                PROJECT_PATH_AGREES := true
-                BOARD_SEPOLICY_DIRS += bootable/recovery-twrp/sepolicy
-            endif
-        endif
-    endif
-endif
-
-ifeq ($(PROJECT_PATH_AGREES),true)
-
 ifeq ($(CM_PLATFORM_SDK_VERSION),)
     CM_PLATFORM_SDK_VERSION := 0
 endif
@@ -824,8 +803,6 @@ endif
 # FB2PNG
 ifeq ($(TW_INCLUDE_FB2PNG), true)
     include $(commands_TWRP_local_path)/fb2png/Android.mk
-endif
-
 endif
 
 commands_TWRP_local_path :=
